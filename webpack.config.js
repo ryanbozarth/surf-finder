644cc15fcb2d298e6f2591749c838c4cd5ba9e10
Ryan Bozarth <ryan_bozarth@ryan-bozarth.lan>
2017-03-11 14:59:30 -0800

    added build

--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,30 @@ module.exports = {
     publicPath: 'public',
     filename: 'bundle.js'
   },
+  devtool: 'source-map',
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+       test: /\.scss$/,
+       loaders: ['style-loader', 'css-loader', 'sass-loader']
+     }
+    ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './style')]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './public'
   }
 };
